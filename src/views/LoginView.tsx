487b294fb703807468a26a3f57f844819445fdcf
Ever-VC <ever.vasquezc27@gmail.com>
2025-01-15 22:06:48 -0600

    🚧 Work in Progress:  Base de inicio de sesión

--- src/views/LoginView.tsx
+++ src/views/LoginView.tsx
@@ -3,11 +3,9 @@ import { Link } from 'react-router-dom'
 export default function LoginView() {
   return (
     <>
-        <div>
-          
-        </div>
-        <nav>
-            <p>¿Aun no tienes una cuenta? <Link to="/auth/register">Registrarse.</Link></p>
+		<h1 className='text-4xl text-white font-bold'>Iniciar Sesión</h1>
+        <nav className='mt-10'>
+            <p className='text-center text-white text-lg block'>¿Aun no tienes una cuenta? <Link to="/auth/register">Registrarse.</Link></p>
         </nav>
     </>
   )
