ba13df3c2842b34ecaef94070708f8be23bc8a5f
Ever-VC <ever.vasquezc27@gmail.com>
2025-01-16 21:22:34 -0600

    🎨 Improved formatting: Formato de 4 espacios al código

--- src/components/ErrorMessage.tsx
+++ src/components/ErrorMessage.tsx
@@ -3,7 +3,7 @@ type ErrorMessageProps = {
 };
 
 export default function ErrorMessage({children} : ErrorMessageProps) {
-  return (
-    <p className="bg-red-50 text-red-600 p-3 uppercase text-sm font-bold text-center">{children}</p>  
-  )
+	return (
+		<p className="bg-red-50 text-red-600 p-3 uppercase text-sm font-bold text-center">{children}</p>  
+	)
 }
